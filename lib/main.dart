4720872aac69fe7fccbf255d097a5f9b6e95ed53
guille <you@example.com>
2026-03-05 19:53:10 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  TextEditingController c1 = TextEditingController();
+  MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ejemplo"),
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+          leading: IconButton(onPressed: (){}, icon: Icon(Icons.person)),
+          actions: [IconButton(onPressed: (){}, icon:Icon(Icons.person))],
+        ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: c1,
+                decoration: InputDecoration(
+                  labelText: "Nombre",
+                  hintText: "Nombre",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15))
+                  )
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(onPressed: (){
+                  print(c1.text);
+                }, 
+                child: Text("Guardar")
+                ),
+                ),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
